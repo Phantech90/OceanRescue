1a688903031da538bf090b4d8eb62053bc47e6d9
s200361 <s200361@ADL-49F0EE>
2020-03-16 20:11:28 +1030

    Made ocean rescue

---
+++ Assets/Scripts/Boat/RescueBoat.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueBoat : MonoBehaviour
+{
+
+    public GameObject m_passenger = null;
+    private float m_timer = 0;
+    public float m_rescueTime = 3;
+    public float m_dropoffTime = 3;
+
+    private bool m_hasPassenger = false;
+
+    private void Start()
+    {
+        m_passenger.SetActive(false);
+        m_hasPassenger = false;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //if you get near a swimmer and you dont have a passenger the timer is reset
+        if (collider.gameObject.tag == "Swimmer" && m_hasPassenger == false)
+        {
+            m_timer = 0;
+        }
+        //if you enter the dropzone and you have a passenger then the timer is reset
+        else if (collider.gameObject.tag == "DropZone" && m_hasPassenger == true)
+        {
+            m_timer = 0;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Swimmer" && m_hasPassenger == false)
+        {
+            m_timer += Time.deltaTime;
+            Debug.Log((m_rescueTime / m_timer));
+            if (m_timer >= m_rescueTime)
+            {
+                PickupSwimmer(collider.gameObject);
+            }
+        }
+        else if (collider.gameObject.tag == "DropZone" && m_hasPassenger == true)
+        {
+            m_timer += Time.deltaTime;
+            if (m_timer >= m_dropoffTime)
+            {
+                DropoffSwimmer();
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_timer = 0;
+    }
+
+    public void PickupSwimmer(GameObject swimmer)
+    {
+        swimmer.SetActive(false);
+        m_passenger.SetActive(true);
+        m_hasPassenger = true;
+    }
+
+    public void DropoffSwimmer()
+    {
+        m_passenger.SetActive(false);
+        m_hasPassenger = false;
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text m_MessageText;
+    public Text m_TimerText;
+    public GameObject[] m_swimmers;
+    public GameObject m_passenger;
+
+    private float m_startTimer = 3;
+    private float m_gameTime = 0;
+    public float GameTime { get { return m_gameTime; } }
+
+    public enum GameState
+    {
+        Start,
+        Playing,
+        GameOver
+    };
+
+    private GameState m_GameState;
+    public GameState State { get { return m_GameState; } }
+
+    private void Start()
+    {
+        OnNewGame();
+    }
+
+    public void OnNewGame()
+    {
+        m_GameState = GameState.Start;
+
+        m_TimerText.text = "";
+        m_MessageText.text = "";
+
+        m_passenger.SetActive(false);
+
+        for (int i = 0; i < m_swimmers.Length; i++)
+        {
+            m_swimmers[i].SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        switch (m_GameState)
+        {
+            case GameState.Start:
+                m_startTimer -= Time.deltaTime;
+
+                m_MessageText.text = "Game starts in " + (int)(m_startTimer + 1);
+
+                if (Input.GetKeyUp(KeyCode.Return) == true || m_startTimer < 0)
+                {
+                    m_gameTime = 0;
+                    m_startTimer = 3;
+                    m_MessageText.text = "";
+                    m_GameState = GameState.Playing;
+                }
+                break;
+            case GameState.Playing:
+                if (AllRescued() == true)
+                {
+                    m_GameState = GameState.GameOver;
+                    m_MessageText.text = "You Win";
+                }
+                else
+                {
+                    m_gameTime += Time.deltaTime;
+                    int seconds = Mathf.RoundToInt(m_gameTime);
+
+                    m_TimerText.text = string.Format("{0:D2}:{1:D2}",
+                                                (seconds / 60), (seconds % 60));
+                }
+                break;
+            case GameState.GameOver:
+                if (Input.GetKeyUp(KeyCode.Return) == true)
+                {
+                    OnNewGame();
+                }
+                break;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private bool AllRescued()
+    {
+        int numRescuedLeft = 0;
+
+        for (int i = 0; i < m_swimmers.Length; i++)
+        {
+            if (m_swimmers[i].activeSelf == true)
+            {
+                numRescuedLeft++;
+            }
+        }
+
+        if (numRescuedLeft <= 0 && m_passenger.activeSelf == false)
+            return true;
+
+        return false;
+    }
+}
